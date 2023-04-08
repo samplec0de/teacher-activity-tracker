902ea35f909d64b6f0e664ba2848fee06f4aa05e
Andrew <andrewmosk@icloud.com>
2023-04-08 20:41:03 +0300

    Первые наброски по телеге

--- src/bot.py
+++ src/bot.py
@@ -1,21 +1,33 @@
-import asyncio
+import logging
 
-from activity.activity_factory import ActivityFactory
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.filters import CommandStart
+from aiogram.utils import executor
+
 from database import create_pool
-from lesson.pg_lesson import PGLesson
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token='5952854813:AAFemh5A5MbK_EBZB8p7BBjDnYvWpjav-Eo')
+dp = Dispatcher(bot)
+
+asyncpg_pool = create_pool()
 
 
-async def main():
-    pool = await create_pool()
-    pg_lesson = PGLesson(2, pool)
-    topic = await pg_lesson.topic
-    print(topic)
-    activity_factory = ActivityFactory(pool)
-    # new_activity = await activity_factory.create(name='Test activity', lesson_id=pg_lesson.id)
-    # print(new_activity.id)
-    activities = await pg_lesson.activities
-    print(activities)
+
+@dp.message_handler(CommandStart())
+async def start_command(message: types.Message):
+    username = message.from_user.first_name
+    # Получаем аргументы команды
+    parameter_value = message.get_args()
+    # Если есть аргументы, отправляем сообщение с ними
+    if parameter_value:
+
+        await message.reply(f"Привет, {username}! Активирую код {parameter_value}.")
+    # Если аргументов нет, отправляем обычное сообщение
+    else:
+        await message.reply("Привет, {username}! Перейди по специальной ссылке для подключения к курсу.")
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    executor.start_polling(dp, skip_updates=False)
